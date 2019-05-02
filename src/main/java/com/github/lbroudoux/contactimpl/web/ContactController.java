834cb3e9b446ba261543fc54636f23ad30a26df3
lbroudoux <laurent.broudoux@gmail.com>
2019-05-02 10:31:02 +0000

    master: Create contact should return 201

--- src/main/java/com/github/lbroudoux/contactimpl/web/ContactController.java
+++ src/main/java/com/github/lbroudoux/contactimpl/web/ContactController.java
@@ -35,7 +35,7 @@ public class ContactController {
     @RequestMapping(value = "/{userId}/contacts", method = RequestMethod.POST)
     public ResponseEntity<Contact> createContact(@RequestBody Contact contact, @PathVariable("userId") String userId) {
         if (contact != null) {
-            return new ResponseEntity<>(contact, HttpStatus.OK);
+            return new ResponseEntity<>(contact, HttpStatus.CREATED);
         }
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
