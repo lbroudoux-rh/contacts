997817cda5c874be8fa472787b9a52d9df3357c2
lbroudoux <laurent.broudoux@gmail.com>
2019-05-02 18:30:45 +0200

    Removing emails

--- src/main/java/com/github/lbroudoux/contactimpl/web/ContactController.java
+++ src/main/java/com/github/lbroudoux/contactimpl/web/ContactController.java
@@ -1,6 +1,7 @@
 package com.github.lbroudoux.contactimpl.web;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,18 +22,18 @@ public class ContactController {
     private static List<Contact> contacts = new ArrayList<>();
 
     static {
-      contacts.add(new Contact("Laurent", "Broudoux", "lbroudou@redhat.com", "123456789"));
-      contacts.add(new Contact("Yacine", "Kheddache", "yacine@redhat.com", "987654321"));
-      contacts.add(new Contact("Patrice", "Lachance", "patrice.lachance@socgen.com", "azertyuiop"));
-      contacts.add(new Contact("Jaafar", "Chraibi", "jchraibi@redhat.com", "qsdfghjklm"));
+      contacts.add(new Contact("Laurent", "Broudoux", "laurent@example.com", "123456789"));
+      contacts.add(new Contact("Yacine", "Kheddache", "yacine@example.com", "987654321"));
+      contacts.add(new Contact("Patrice", "Lachance", "patrice@example.com", "azertyuiop"));
+      contacts.add(new Contact("Jaafar", "Chraibi", "jaafar@example.com", "qsdfghjklm"));
     }    
 
-    @RequestMapping(value = "/{userId}/contacts", method = RequestMethod.GET)
+    @RequestMapping(value = "/{userId}/contacts", method = RequestMethod.GET, produces = "application/json")
     public List<Contact> getContacts(@PathVariable("userId") String userId) {
         return contacts;
     }
 
-    @RequestMapping(value = "/{userId}/contacts", method = RequestMethod.POST)
+    @RequestMapping(value = "/{userId}/contacts", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<Contact> createContact(@RequestBody Contact contact, @PathVariable("userId") String userId) {
         if (contact != null) {
             return new ResponseEntity<>(contact, HttpStatus.OK);
